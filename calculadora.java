bc927cf2d165230d0640ab0e9105d2bcdfd4c0d8
jhoan07u <arturomarinmaigual@gmail.com>
2026-07-16 11:27:03 -0500

    dividion

--- calculadora.java
+++ calculadora.java
@@ -5,5 +5,8 @@ public class Calculadora {
     public double sumar(double num1, double num2) {
         return num1 + num2;
     }
+     public double division(double num1, double num2) {
+        return num1 - num2;
+    }
 
 }
