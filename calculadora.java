23b4bf2f26e74a5f2bdf2a72b9e7e3509ccca5de
Juan Marcos Caicedo Mejía <46875325+juanmarcosdev@users.noreply.github.com>
2026-07-16 11:43:16 -0500

    Fix arithmetic operations in calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -3,14 +3,14 @@ public class Calculadora {
     //completen :)
     // Método para sumar dos números
     public double sumar(double num1, double num2) {
-        return num1 + num2;
+        return num1 * num2;
     }
      public double division(double num1, double num2) {
-        return num1 - num2;
+        return num1 / num2;
 
     }
     public double multiplicacion(double num1, double num2) {
-        return num1 * num2;
+        return num1 + num2;
         
     }
     //metodo para resta
